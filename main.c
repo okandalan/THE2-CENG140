092b777ddb3c94182c811a6232f871b517ef9424
Okan <okan.dalan11@gmail.com>
2022-04-06 20:58:58 +0300

    deleting unnecessary comments

--- main.c
+++ main.c
@@ -1,16 +1,6 @@
 #include "functions.h"
 
-/*
-You will not submit this file, just use for testing your code.
-All your implementations should be in functions.c
-*/
 
-/*
-To compile your code, use "make main"
-Then "./main" to run.
-If you want to compile again, you need to delete the previous executable.
-You may do this by "make clean" or simply "rm main"
-*/
 
 int main(){
     int  i, j;
@@ -114,20 +104,5 @@ int main(){
     season_rank = find_season_ranking(total_points, drivers, 9);
     printf("%u\n", season_rank);
 
-
-
-
-
-    /*
-    Function calls here to test your code
-    Example:
-    float ***sector_times, **laptimes;
-    unsigned int fastest_lap, **positions;
-    sector_times = create_sector_times(...);
-    positions = create_positions(...);
-    lap_times = calculate_lap_times(...);
-    fastest_lap = find_fastest_lap(lap_times, ...);
-    printf("Printing fastest lap to check the answer: %d\n", fastest_lap);
-    */
     return 0;
 }
